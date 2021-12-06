228b828f8b35a9c1ff04d666b7bab1a56579d346
hermessuen <hermessuen@gmail.com>
2021-12-06 18:02:30 -0500

    Update ml_sample_input.py

--- Streamlit/ml_sample_input.py
+++ Streamlit/ml_sample_input.py
@@ -20,10 +20,77 @@ def run():
     st.set_page_config(layout="wide")
     st.title("Predicting Happiness with Machine Learning")
 
+    # Read dependecies
+    df = pd.read_csv('./Data/cleaned_whr.csv')
+    df['year'] = df['year'].astype('int64')
+    features = ['year', 'Log GDP per capita',
+                'Social support', 'Healthy life expectancy at birth',
+                'Freedom to make life choices', 'Generosity',
+                'Perceptions of corruption']
+    scaler = MinMaxScaler()
+    scaler.fit(df[features])
+
+    with open('Models/rf.pkl', 'rb') as f:
+        rf = pickle.load(f)
+
+    sample_id = np.random.randint(0, high=len(df))
+
+    # Intro
+    st.header('Happiness Score Prediction')
     st.write("We trained three interpretable models on our dataset: Linear Regression, "
              "Decision Tree, and Random Forest. Each of these models are inherently interpretable."
              " Thus, we are able to determine the importance of each feature on our prediction for happiness"
              "for each of the three models")
+    st.write("Of all the models we trained, the Random Forest model had the highest R squared value (0.86) and we will now "
+             "demonstrate its performance on example data from our training set")
+
+    # pull up the button here
+    # add caption: on what you can do with the button
+    # all three models and compare
+
+    def get_pred_score(feat_vals):
+        scaled_feat = scaler.transform(feat_vals.values.reshape(1, -1))[0]
+        scaled_feat = scaled_feat.reshape(1, -1)
+        return rf.predict(scaled_feat)[0]
+
+    if st.button('Get new example'):
+        sample_id = np.random.randint(0, high=len(df))
+    country, year = df['Country name'].iloc[sample_id], df[features[0]].iloc[sample_id]
+    st.markdown(f'<h4>{country} in {year} </h4>', unsafe_allow_html=True)
+
+    feat_vals = df[features].iloc[sample_id]
+    feat_vals.name = ' '
+    score = df['Life Ladder'].iloc[sample_id]
+
+    pred_score = get_pred_score(feat_vals)
+
+    st.table(feat_vals[1:])  # [1:] to avoid year
+    st.write(f'Actual Happiness Score: {score}/10')
+    st.write(f'Predicted Happiness Score: {np.around(pred_score, decimals=3)}/10')
+
+    st.header("Try it yourself!")
+
+    with st.form(key="test"):
+        year = st.select_slider(label="year", options=np.array(np.arange(2005, 2022)))
+        gdp = st.slider(label="Log GDP Per Capita", min_value=6.0, max_value=12.0, value=6.0, step=0.1)
+        social_support = st.slider(label="Social Support", min_value=0.0, max_value=1.0, value=0.0, step=0.01)
+        life_expect = st.slider(label="Life Expectancy", min_value=30.0, max_value=100.0, value=30.0, step=0.5)
+        freedom = st.slider(label="Freedom to Make life Choice", min_value=0.0, max_value=1.0, value=0.0, step=0.01)
+        generosity = st.slider(label="Genorosity", min_value=-0.4, max_value=1.0, value=-0.4, step=0.01)
+        corruption = st.slider(label="Perception of Corruption", min_value=0.0, max_value=1.0, value=0.0, step=0.01)
+
+        X = np.array([year, gdp, social_support, life_expect, freedom, generosity, corruption]).reshape(1, -1)
+        X = scaler.transform(X)
+        y_hat_rf = str(round(rf.predict(X)[0], 3))
+        y_hat_dt = str(round(clf_tree.predict(X)[0], 3))
+        y_hat_lr = str(round(linreg.predict(X)[0][0], 3))
+        submit_form = st.form_submit_button(label='Try Our model!')
+        if submit_form:
+            st.success("Random Forest predicted {0}".format(y_hat_rf))
+            st.success("Linear Regression predicted {0}".format(y_hat_lr))
+            st.success("Decision Tree predicted {0}".format(y_hat_dt))
+
+
 
 
 
@@ -59,7 +126,7 @@ def run():
 
     col3, col4 = st.columns(2)
     col3.write(decision_tree_df)
-    col4.image("plots/dt_pic.png")
+    col4.image("plots/dt_pic.png", width=300)
 
 
     st.caption(
@@ -70,74 +137,8 @@ def run():
     col5.write(random_forest_df)
     col6.image("plots/rf_performance.png")
 
-    # Read dependecies
-    df = pd.read_csv('./Data/cleaned_whr.csv')
-    df['year'] = df['year'].astype('int64')
-    features = ['year', 'Log GDP per capita',
-                'Social support', 'Healthy life expectancy at birth',
-                'Freedom to make life choices', 'Generosity',
-                'Perceptions of corruption']
-    scaler = MinMaxScaler()
-    scaler.fit(df[features])
-
-    with open('Models/rf.pkl', 'rb') as f:
-        rf = pickle.load(f)
-
-    sample_id = np.random.randint(0, high=len(df))
-
-    # Intro
-    st.header('Happiness Score Prediction')
-    st.write("The Random Forest model had the highest R squared value (0.86) and we will now "
-             "demonstrate its performance on example data from our training set")
-
-    # pull up the button here
-    # add caption: on what you can do with the button 
-    # all three models and compare 
-
-    def get_pred_score(feat_vals):
-        scaled_feat = scaler.transform(feat_vals.values.reshape(1, -1))[0]
-        scaled_feat = scaled_feat.reshape(1, -1)
-        return rf.predict(scaled_feat)[0]
-
-    if st.button('Get new example'):
-        sample_id = np.random.randint(0, high=len(df))
-    country, year = df['Country name'].iloc[sample_id], df[features[0]].iloc[sample_id]
-    st.markdown(f'<h4>{country} in {year} </h4>', unsafe_allow_html=True)
-
-    feat_vals = df[features].iloc[sample_id]
-    feat_vals.name = ' '
-    score = df['Life Ladder'].iloc[sample_id]
-
-    pred_score = get_pred_score(feat_vals)
-
-    st.table(feat_vals[1:])  # [1:] to avoid year
-    st.write(f'Actual Happiness Score: {score}/10')
-    st.write(f'Predicted Happiness Score: {np.around(pred_score, decimals=3)}/10')
 
 
-
-    st.header("Try it yourself!")
-
-    with st.form(key="test"):
-        year = st.select_slider(label="year", options=np.array(np.arange(2005, 2022)))
-        gdp = st.slider(label="Log GDP Per Capita", min_value=6.0, max_value=12.0, value=6.0, step=0.1)
-        social_support = st.slider(label="Social Support", min_value=0.0, max_value=1.0, value=0.0, step=0.01)
-        life_expect = st.slider(label="Life Expectancy", min_value=30.0, max_value=100.0, value=30.0, step=0.5)
-        freedom = st.slider(label="Freedom to Make life Choice", min_value=0.0, max_value=1.0, value=0.0, step=0.01)
-        generosity = st.slider(label="Genorosity", min_value=-0.4, max_value=1.0, value=-0.4, step=0.01)
-        corruption = st.slider(label="Perception of Corruption", min_value=0.0, max_value=1.0, value=0.0, step=0.01)
-
-        X = np.array([year, gdp, social_support, life_expect, freedom, generosity, corruption]).reshape(1, -1)
-        X = scaler.transform(X)
-        y_hat_rf = str(round(rf.predict(X)[0], 3))
-        y_hat_dt = str(round(clf_tree.predict(X)[0], 3))
-        y_hat_lr = str(round(linreg.predict(X)[0][0], 3))
-        submit_form = st.form_submit_button(label='Try Our model!')
-        if submit_form:
-            st.success("Random Forest predicted {0}".format(y_hat_rf))
-            st.success("Linear Regression predicted {0}".format(y_hat_lr))
-            st.success("Decision Tree predicted {0}".format(y_hat_dt))
-            
 
 if __name__ == "__main__":
     run()
