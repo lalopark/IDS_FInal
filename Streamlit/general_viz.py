397335e6f1018fe2b116aa754df2376863ecaf5e
Ben <ben.sukboontip@gmail.com>
2021-12-06 18:09:47 -0500

    general viz

--- Streamlit/general_viz.py
+++ Streamlit/general_viz.py
@@ -11,6 +11,7 @@ pio.templates.default = "plotly_white"
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly import figure_factory as ff
+import altair as alt
 import pycountry
 
 
@@ -27,59 +28,99 @@ def run():
     df = pd.read_csv(path)
     # st.set_page_config(layout="wide")
 
+    st.markdown("#### **Select Year:**")
+
+    min_year = df['year'].min()
+    max_year = df['year'].max()
+    year = st.slider("Year", min_year, max_year, value=max_year)
+
+    year_filtered = df[df["year"] == year]
+
+    features = ['Log GDP per capita', 'Social support', 'Healthy life expectancy at birth', 'Freedom to make life choices', 'Generosity', 'Perceptions of corruption']
+    x_feature = st.selectbox("Select a feature for the x axis", features, index=0)
+    y_feature = st.selectbox("Select a feature for the y axis", features, index=1)
+
+    fig = alt.Chart(year_filtered).mark_circle(size=60).encode(
+        x=x_feature,
+        y=y_feature,
+        color='Life Ladder',
+        tooltip=['Country name', 'Life Ladder'] + features
+        ).interactive()
+
+    st.altair_chart(fig, use_container_width=True)
 
     # top 10 bottom 10
-    df_2021 = df[df['year']==2021]
+    df_2021 = df[df['year']==year]
     df_2021 = df_2021.sort_values(by=["Life Ladder"], ascending=False)
-    topk = botk = 20
+    topk = botk = 10
     top = df_2021.iloc[:topk]
     bot = df_2021.iloc[-botk:]
 
     top_desc = top.sort_values(by=['Life Ladder'], ascending=True)
     fig = px.bar(top_desc, y="Country name", x="Life Ladder", orientation='h',color="Life Ladder",color_continuous_scale='RdPu')
-    st.subheader('Top 10 Happiest Countries in 2021')
-    st.plotly_chart(fig) 
+    st.subheader(f'Top 10 Happiest Countries in {year}')
+    st.plotly_chart(fig, use_container_width=True) 
 
     bot_asc = bot.sort_values(by=['Life Ladder'], ascending=False)
     fig2 = px.bar(bot_asc, y="Country name", x="Life Ladder", orientation='h',color="Life Ladder",color_continuous_scale='PuBu')
-    st.subheader('Top 10 Unhappiest Countries in 2021')
-    st.plotly_chart(fig2) 
+    st.subheader(f'Top 10 Unhappiest Countries in {year}')
+    st.plotly_chart(fig2, use_container_width=True) 
 
     # score over year
     # BEN: create a year slider (2005 - 2021) 
      
     # include 2005 
+    
     year_filtered_df = df[df['year'] != 2005]
     avg_happiness = pd.DataFrame(year_filtered_df.groupby(['year'])['Life Ladder'].mean()).reset_index()
 
     fig3 = px.line(avg_happiness, x="year", y="Life Ladder")
     st.subheader('Average Happiness over time since 2006')
-    st.plotly_chart(fig3) 
+    st.plotly_chart(fig3, use_container_width=True) 
 
     # histogram
-    fig8 = px.histogram(df_2021, x= 'Life Ladder', nbins = 50)
-    fig8.update_layout(bargap=0.2)
-    st.subheader('Happiness Score Distribution for 2021')
-    st.plotly_chart(fig8) 
+    st.markdown("#### **Select Year for Histogram:**")
 
-    # ben: aattempt slider 
+    min_year = df['year'].min()
+    max_year = df['year'].max()
+    year_hist = st.slider("Year", min_year, max_year, value=max_year, key="hist")
+
+    year_filtered = df[df["year"] == year_hist]
+
+    # fig8 = px.histogram(df_2021, x= 'Life Ladder', nbins = 50)
+    fig8 = px.histogram(year_filtered, x= 'Life Ladder', nbins = 50)
+    fig8.update_layout(bargap=0.2)
+    st.subheader(f'Happiness Score Distribution for {year_hist}')
+    st.plotly_chart(fig8, use_container_width=True) 
+
+    # ben: attempt slider 
 
     #if not, country map: include 2005 
     # su: change color to pink
 
     # country map 
+    st.markdown("#### **Select Year for to Visualize Map:**")
+
+    min_year = df['year'].min()
+    max_year = df['year'].max()
+    year_map = st.slider("Year", min_year, max_year, value=max_year, key="map")
+
+    year_filtered = df[df["year"] == year_map]
+
     countries = {}
     for country in pycountry.countries:
         countries[country.name] = country.alpha_3
 
-    codes = [countries.get(country, 'Unknown code') for country in df_2021['Country name']]
+    # codes = [countries.get(country, 'Unknown code') for country in df_2021['Country name']]
+    codes = [countries.get(country, 'Unknown code') for country in year_filtered['Country name']]
 
-    df_2021['codes'] = codes
+    year_filtered['codes'] = codes
+    # df_2021['codes'] = codes
 
-    fig7 = px.choropleth(df_2021, locations="codes",
+    fig7 = px.choropleth(year_filtered, locations="codes",
                         color="Life Ladder", # lifeExp is a column of gapminder
                         hover_name="Country name", # column to add to hover information
-                        color_continuous_scale=px.colors.sequential.RdBu_r)
+                        color_continuous_scale="Sunset")
     fig7.update_layout(
         autosize=False,
         margin = dict(
@@ -93,33 +134,33 @@ def run():
         width=1500,
         #     height=400,
     )
-    st.subheader('Happiness Score (Life Ladder) in 2021')
+    st.subheader(f'Happiness Score (Life Ladder) in {year_map}')
     st.plotly_chart(fig7, use_container_width=True) 
 
 
 
 
     # correlation amongst features: all 
-    # make color schemees consistnet (light vs dark)
+    # make color schemes consistent (light vs dark)
 
     # extend to 20 
     df_corr = df.drop('year',1).corr().round(2)
     fig4 = ff.create_annotated_heatmap(z=df_corr.to_numpy(), 
                                     x=df_corr.columns.tolist(),
                                     y=df_corr.columns.tolist(),
-                                    colorscale=px.colors.sequential.Agsunset,
+                                    colorscale="RdBu",
                                     hoverinfo="none", #Shows hoverinfo for null values
                                     showscale=True, ygap=1, xgap=1
                                     )
     st.subheader('Correlation Matrix Amongst Features')
-    st.plotly_chart(fig4) 
+    st.plotly_chart(fig4, use_container_width=True) 
 
     # correlation amongst features: happy 
     top_corr = top.drop('year',1).corr().round(2)
     fig5 = ff.create_annotated_heatmap(z=top_corr.to_numpy(), 
                                     x=top_corr.columns.tolist(),
                                     y=top_corr.columns.tolist(),
-                                    colorscale=px.colors.sequential.RdPu,
+                                    colorscale="RdBu",
                                     hoverinfo="none", #Shows hoverinfo for null values
                                     showscale=True, ygap=1, xgap=1
                                     )
@@ -134,7 +175,7 @@ def run():
     fig6 = ff.create_annotated_heatmap(z=bot_corr.to_numpy(), 
                                     x=bot_corr.columns.tolist(),
                                     y=bot_corr.columns.tolist(),
-                                    colorscale=px.colors.sequential.PuBu,
+                                    colorscale="RdBu",
                                     hoverinfo="none", #Shows hoverinfo for null values
                                     showscale=True, ygap=1, xgap=1
                                     )
