3934301c51edba7202830a83c9818e78057c7b4a
lalopark <92047634+lalopark@users.noreply.github.com>
2021-12-06 23:57:12 -0500

    Update dev_contact.py

--- Streamlit/dev_contact.py
+++ Streamlit/dev_contact.py
@@ -4,7 +4,7 @@ def run():
     st.markdown("# Developers involved in this project")
 
     st.markdown("Jeffery Cao Siming: jeffery2@andrew.cmu.edu")
-    st.markdown("Su Min Park: suminpar@andrew.cmu.edu")
-    st.markdown("Hermas Suen: hsuen@andrew.cmu.edu")
+    st.markdown("Su Park: suminpar@andrew.cmu.edu")
+    st.markdown("Hermes Suen: hsuen@andrew.cmu.edu")
     st.markdown("Ben Sukboontip: bsukboon@andrew.cmu.edu")
     st.markdown("Jingyi Zhang: jingyiz4@andrew.cmu.edu")
